d4dfa872ba7157a627cde9a28235af7f5a03ded4
nicole-deng4 <nd6753@princeton.edu>
2025-03-20 13:21:31 -0400

    Update symtablelist.c

--- symtablelist.c
+++ symtablelist.c
@@ -47,20 +47,108 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue) {
 
   if (!SymTable_contains (oSymTable, pcKey)) {
     Node* newNode = (Node*) malloc (sizeOf(Node));
+    
     if (newNode == NULL)
     {
-      return NULL;
+      return 0;
     }
+    
     newNode -> key = malloc (strlen (pcKey) + 1);
+    
     if (newNode -> key == NULL) {
         free (newNode);
         return 0;
     }
+    
     strcpy(newNode->key, pcKey);
-    newNode->value = (void *)pvValue;
-    newNode->next = oSymTable->head;
-    oSymTable->head = newNode;
-    oSymTable->length++;
+    newNode -> value = (void *)pvValue;
+    newNode -> next = oSymTable -> head;
+    oSymTable -> head = newNode;
+    oSymTable -> length++;
     return 1;
+    }
+  else {
+    return 0;
   }
+}
 
+void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvValue) {
+  assert (oSymTable);
+  assert (pcKey);
+
+  Node *currNode = OSymTable -> head;
+  while (currNode != NULL) {
+    if (strcmp (currNode -> key, pcKey) == 0)
+    {
+      void *ogValue = currNode -> value;
+      curr -> value = (void *)pvValue;
+      return ogValue;
+    }
+    currNode = currNode -> next;
+  }
+  return NULL;
+}
+
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey) {
+  assert (oSymTable);
+  assert (pcKey);
+
+  Node *currNode = OSymTable -> head;
+  while (currNode != NULL) {
+    if (strcmp (currNode -> key, pcKey) == 0)
+    {
+      return 1;
+    }
+    currNode = currNode -> next;
+  }
+  return NULL;
+}
+
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey) {
+  assert (oSymTable);
+  assert (pcKey);
+  
+  Node *currNode = OSymTable -> head;
+  while (currNode != NULL) {
+    if (strcmp (currNode -> key, pcKey) == 0)
+    {
+      return currNode -> value;
+    }
+    currNode = currNode -> next;
+  }
+  return NULL;
+}
+
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey) {
+  assert (oSymTable);
+  assert (pcKey);
+  
+  Node *currNode = oSymTable -> head;
+  Node *prevNode = NULL;
+    
+  while (currNode != NULL) {
+    if (strcmp(currNode -> key, pcKey) == 0) {
+      void *currValue = current -> value;
+      
+      if (prevNode != NULL) {
+        prevNode -> next = currNode -> next;
+      }
+      else {
+        oSymTable -> head = currNode -> next;
+      }
+      
+      free(currNode -> key);
+      free(currNode);
+      oSymTable -> length--;
+      return currValue;
+    }
+    else {
+      prevNode = currNode;
+      currNode = currNode -> next;
+    }
+return NULL;   
+}
+
+void SymTable_map(SymTable_T oSymTable, void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra), const void *pvExtra) {
+  
+}
